c05033c12319c7c8e754f98f0a304a6495141cc1
Jake <git@ja-ke.tech>
2019-01-18 00:37:25 +0100

    Add request logging

--- convert.js
+++ convert.js
@@ -2,13 +2,18 @@ const fs = require("fs");
 const JSON5 = require('json5')
 const express = require('express')
 const app = express()
+const port = 3000
 
 const json = fs.readFileSync("keys.json");
 const shortcuts = JSON5.parse(json)
 
 console.log(Object.keys(shortcuts))
 
-const port = 3000
+// Log all requests
+app.use((req, res, next) => {
+    console.log(`${process.uptime()}: ${req.method} ${req.url} received from ${req.headers.host}`)
+    next()
+})
 
 app.get('/', (req, res) => res.send('no World!'))
 
