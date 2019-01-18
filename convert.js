12c5399cd83acc13c545058e4c62d230f4d96505
Jake <git@ja-ke.tech>
2019-01-18 01:06:09 +0100

    Install mustache

--- convert.js
+++ convert.js
@@ -1,7 +1,10 @@
 const fs = require("fs");
 const JSON5 = require('json5')
 const express = require('express')
+const mustacheExpress = require('mustache-express');
 const app = express()
+app.engine('mustache', mustacheExpress());
+app.set('view engine', 'mustache')
 const port = 3000
 
 const json = fs.readFileSync("keys.json");
@@ -15,6 +18,7 @@ app.use((req, res, next) => {
     next()
 })
 
-app.get('/', (req, res) => res.send('no World!'))
+
+app.get('/', (req, res) => res.render('index', {name: ['Peter', 'Martin']}))
 
 app.listen(port, () => console.log(`App started at http://0.0.0.0:${port}`))
