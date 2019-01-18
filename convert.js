84919f424cffd699d5e8a9a2a6200b4b5af28242
Jake <git@ja-ke.tech>
2019-01-18 01:43:31 +0100

    Preprocess data

--- convert.js
+++ convert.js
@@ -7,17 +7,38 @@ app.engine('mustache', mustacheExpress());
 app.set('view engine', 'mustache')
 const port = 3000
 
+// Parse config to object
 const json = fs.readFileSync("keys.json");
 const shortcuts = JSON5.parse(json)
 
-console.log(Object.keys(shortcuts))
+// Build object with categories
+let cat = Object.keys(shortcuts).map(val => shortcuts[val].category)
+cat = [... new Set(cat)]
+let viewData = cat.reduce((prev, val) => {
+    prev[val] = []
+    return prev
+}, {})
+
+// Populate categories with data
+for (const key in shortcuts) {
+    const item = shortcuts[key]
+    viewData[item.category].push({
+        action: item.title,
+        keys: item.keys
+    })
+}
+// convert back to array of objects
+viewData = Object.keys(viewData).map(key => ({title: key, options: viewData[key]}))
+
+console.log(viewData)
+
+
 
 // Log all requests
 app.use((req, res, next) => {
     console.log(`${process.uptime()}: ${req.method} ${req.url} received from ${req.headers.host}`)
     next()
 })
-
 
 app.get('/', (req, res) => res.render('index', {name: ['Peter', 'Martin']}))
 
